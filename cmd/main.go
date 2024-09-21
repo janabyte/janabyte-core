02ff687f0ca452b040d49773585622404accedd7
aidosgal <agalimzhan928@gmail.com>
2024-09-21 14:54:55 +0500

    feat: add database config

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,27 @@ import (
 	"log"
 
 	"github.com/aidosgal/janabyte/janabyte-core/cmd/janabyte"
+	"github.com/aidosgal/janabyte/janabyte-core/config"
+	"github.com/aidosgal/janabyte/janabyte-core/database"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-    server := janabyte.NewApiServer(":8080", nil)
+    db, err := database.NewMySQLStorage(mysql.Config{
+        User:                   config.Envs.DBUser,
+        Passwd:                 config.Envs.DBPassword,
+        Addr:                   config.Envs.DBAddress,
+        DBName:                 config.Envs.DBName,
+        Net:                    "tcp",
+        AllowNativePasswords:   true,
+        ParseTime:              true,
+    })
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    server := janabyte.NewApiServer(":8080", db)
 
     if err := server.Run(); err != nil {
         log.Fatal(err)
